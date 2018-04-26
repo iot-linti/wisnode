d0d45e7b1453a9f8bd929e5c97803166f7218d76
Emilio Crudele <ecrudele@users.noreply.github.com>
2018-04-26 15:32:13 -0300

    agregado template para abp

---
+++ secrets_abp_template.py
@@ -0,0 +1,12 @@
+# completar campos y renombrar a secrets_abp.py
+
+# THE_THINGS_NETWORK
+TTN_devADDR = '' # 8 caracteres hex
+TTN_nwksKEY = '' # 32 caracteres hex
+TTN_appsKEY = '' # 32 caracteres hex
+
+
+# Orbiwise
+DASS_devADDR = '' # 8 caracteres hex
+DASS_nwksKEY = '' # 32 caracteres hex
+DASS_appsKEY = '' # 32 caracteres hex
