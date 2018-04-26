41c2b9c0c1c9fc9a7b859b945108f276e6d14d0b
Emilio Crudele <ecrudele@users.noreply.github.com>
2018-04-26 16:56:26 -0300

    agregado devEUI en secrets_abp_template.py

--- secrets_abp_template.py
+++ secrets_abp_template.py
@@ -1,12 +1,14 @@
 # completar campos y renombrar a secrets_abp.py
 
 # THE_THINGS_NETWORK
+TTN_devEUI = '' # 16 caracteres hex
 TTN_devADDR = '' # 8 caracteres hex
 TTN_nwksKEY = '' # 32 caracteres hex
 TTN_appsKEY = '' # 32 caracteres hex
 
 
 # Orbiwise
+DASS_devEUI = '' # 16 caracteres hex
 DASS_devADDR = '' # 8 caracteres hex
 DASS_nwksKEY = '' # 32 caracteres hex
 DASS_appsKEY = '' # 32 caracteres hex
